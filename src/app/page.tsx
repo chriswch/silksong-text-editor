1a20c75aa0b5695e8c02007ce75f8c42724d4e85
chriswch <chriswangwch@gmail.com>
2025-09-23 00:42:22 +0800

    add the dialogue editor to the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
 "use client";
 
+import { DialogueEditor } from "@/components/dialogue-editor";
 import { FileUpload } from "@/components/file-upload";
 import Footer from "@/components/footer";
 import TopNavbar from "@/components/navbar";
+import { useDialogueStore } from "@/hooks/use-dialogue-store";
 
 export default function Home() {
+  const { dialogueData } = useDialogueStore();
+  const hasData = Object.keys(dialogueData).length > 0;
+
   return (
     <div className="min-h-screen bg-background flex flex-col">
       <TopNavbar />
 
       <main className="flex-1 container mx-auto py-6 px-6">
-        <FileUpload />
+        {hasData ? <DialogueEditor /> : <FileUpload />}
       </main>
 
       <Footer />
