170e71ee95fa545d71996cd455a283c599d6a72a
chriswch <chriswangwch@gmail.com>
2025-09-27 15:18:38 +0800

    rust: python runtime decision flow

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,9 @@
 fn main() {
-    tauri_build::build()
+    // Bake the env variables first
+    if let Ok(path) = std::env::var("PYTHON_RUNTIME_PATH") {
+        println!("cargo:rustc-env=PYTHON_RUNTIME_PATH={}", path);
+    }
+
+    // Then build the app
+    tauri_build::build();
 }
